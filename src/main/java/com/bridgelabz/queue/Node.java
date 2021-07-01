73074dcdac724f2b7c9766bb44a799eb029a58b4
Atul <atulgaikarag9@gmail.com>
2021-07-01 18:18:25 +0530

    Queue Using Linked List

---
+++ src/main/java/com/bridgelabz/queue/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.queue;
+
+public class Node {
+
+	int key;
+	Node next;
+
+	public Node(int key) {
+		this.key = key;
+		this.next = null;
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/queue/QueueMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.queue;
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+		QueueOperations op = new QueueOperations();
+
+		op.addQ(56);
+		op.addQ(30);
+		op.addQ(70);
+		op.dequeue();
+		op.dequeue();
+
+		System.out.println("Queue Front : " + op.front.key);
+		System.out.println("Queue Rear : " + op.rear.key);
+	}
+
+}
